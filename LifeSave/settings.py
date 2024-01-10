5252c4d84546919ab6efe6d34069c2937f2810ac
etan-nestor <etannestor45@gmail.com>
2024-01-10 02:15:32 +0000

    new commit again

--- LifeSave/settings.py
+++ LifeSave/settings.py
@@ -14,7 +14,7 @@ TEMPLATES_DIRS=os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'django-insecure-pk9m%_jjw&e&6ji)+qs%7(i=r!5733$@m8=9l66$0=pj6qr61%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static']
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
