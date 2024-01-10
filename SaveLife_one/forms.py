92d1becefbeda43bf5013dd909171183996081a0
etan-nestor <etannestor45@gmail.com>
2024-01-10 01:20:20 +0000

    new commit again

--- SaveLife_one/forms.py
+++ SaveLife_one/forms.py
@@ -4,8 +4,16 @@ from .models import Comment
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = [
-            "name",
-            "email",
-            "body"
-        ]
+        fields = ["name", "email", "body"]
+
+        labels = {
+            "name": "Votre nom",
+            "email": "Email",
+            "body": "Votre commentaire ici",
+        }
+
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control rounded shadow-sm', 'placeholder': 'Your name ', 'style': 'width: 40%;'}),
+            "email": forms.EmailInput(attrs={'class': 'form-control rounded shadow-sm', 'placeholder': 'Your Email', 'style': 'width: 40%;'}),
+            "body": forms.Textarea(attrs={'class': 'form-control rounded shadow-sm', 'placeholder': 'Your comment here !', 'rows': 5, 'style': 'width: 100%;'}),
+        }
